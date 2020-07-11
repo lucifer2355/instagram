137d90921f009f5d23077e64654d06c2425df3ea
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-12 00:00:29 +0530

    add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Post from "./components/Post";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app_header'>
+        <img
+          className='app_header_logo'
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png'
+          alt='logo'
+        />
+      </div>
+      <Post />
     </div>
   );
-}
+};
 
 export default App;
