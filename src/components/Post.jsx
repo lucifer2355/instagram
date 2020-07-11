3e2d06e86d7f807498af832705b6641c17e21119
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-12 00:16:36 +0530

    add MATERIAL-UI

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,7 +10,9 @@ function Post() {
         src='https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png'
         alt=''
       />
-      <h4>Username: caption</h4>
+      <h4 className='post_text'>
+        <strong>Username</strong>caption
+      </h4>
     </div>
   );
 }
